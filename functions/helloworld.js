0470abf1c7c0896675b9a9461e506f8c6d954c36
丁攀峰 <allen@leanktech.com>
2023-08-17 09:45:47 +0800

    feat:update

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -2,9 +2,9 @@
  * @Author: 丁攀峰 allen@leanktech.com
  * @Date: 2023-08-17 09:42:54
  * @LastEditors: 丁攀峰 allen@leanktech.com
- * @LastEditTime: 2023-08-17 09:43:03
+ * @LastEditTime: 2023-08-17 09:45:38
  * @FilePath: /demo/functions/helloworld.js
  */
 export function onRequest(context) {
-    return new Response("Hello, world!")
+    return new Response("Hello, world!,我functions")
   }

c271e560aeccbfad54cfe8471d1cc22abc2aa7b2
丁攀峰 <allen@leanktech.com>
2023-08-17 09:44:16 +0800

    feat:update

--- functions/hello.js
+++
@@ -1,5 +0,0 @@
-export default function handleRequest(request) {
-  return new Response('Hello, world!', {
-    headers: { 'content-type': 'text/plain' },
-  })
-}
---
+++ functions/helloworld.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: 丁攀峰 allen@leanktech.com
+ * @Date: 2023-08-17 09:42:54
+ * @LastEditors: 丁攀峰 allen@leanktech.com
+ * @LastEditTime: 2023-08-17 09:43:03
+ * @FilePath: /demo/functions/helloworld.js
+ */
+export function onRequest(context) {
+    return new Response("Hello, world!")
+  }

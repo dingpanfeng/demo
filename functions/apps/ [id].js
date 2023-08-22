6e0b30cc39b3798634124625aee874155f1b6fab
丁攀峰 <allen@leanktech.com>
2023-08-22 10:33:35 +0800

    feat: 修改名字

--- functions/apps/ [id].js
+++ functions/apps/ [id].js
@@ -2,9 +2,9 @@
  * @Author: 丁攀峰 allen@leanktech.com
  * @Date: 2023-08-22 10:24:50
  * @LastEditors: 丁攀峰 allen@leanktech.com
- * @LastEditTime: 2023-08-22 10:25:50
+ * @LastEditTime: 2023-08-22 10:32:56
  * @FilePath: /demo/functions/apps/ [id].js
  */
 export function onRequest(context) {
-    return new Response(context.params.id)
-  }
+    return new Response(`走到这了嘛[id], ${JSON.stringify(context.params.id)}`)
+}
--- functions/apps/[[catchall]].js
+++ functions/apps/[[catchall]].js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 丁攀峰 allen@leanktech.com
+ * @Date: 2023-08-22 10:25:32
+ * @LastEditors: 丁攀峰 allen@leanktech.com
+ * @LastEditTime: 2023-08-22 10:32:30
+ * @FilePath: /demo/functions/apps/[[catchall]].js
+ */
+
 export function onRequest(context) {
-    return new Response(JSON.stringify(context.params.catchall))
+    return new Response(`走到这了嘛catchall, ${JSON.stringify(context.params.catchall)}`)
 }

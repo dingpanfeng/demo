d0fe9da06121e52f0eefea30545aafe27dfffb5d
丁攀峰 <allen@leanktech.com>
2023-08-22 10:30:05 +0800

    feat: 修改名字

---
+++ functions/apps/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: 丁攀峰 allen@leanktech.com
+ * @Date: 2023-08-22 10:28:57
+ * @LastEditors: 丁攀峰 allen@leanktech.com
+ * @LastEditTime: 2023-08-22 10:29:35
+ * @FilePath: /demo/functions/apps/index.js
+ */
+export function onRequest(context) {
+    return new Response('Hello, world!,我functions，我改写下试试')
+}

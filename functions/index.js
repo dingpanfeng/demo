fb8464e745ec5a201d91b9459833446ab7186c10
丁攀峰 <allen@leanktech.com>
2023-08-29 11:16:23 +0800

    feat: test1

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,6 @@
  * @LastEditTime: 2023-08-29 11:00:43
  * @FilePath: /demo/functions/index.js
  */
-// 这个目录用于存放 Serverless 函数，这些函数通常用于处理特定的 API 请求
 export function onRequest(context) {
     return new Response('Hello, world!,我functions，我改写下试试')
 }

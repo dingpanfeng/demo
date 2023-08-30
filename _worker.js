31a15100bafa15b345149ee1c1db6e04a802ca71
丁攀峰 <allen@leanktech.com>
2023-08-30 14:42:02 +0800

    test10

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@
  * @Author: 丁攀峰 allen@leanktech.com
  * @Date: 2023-08-22 10:49:44
  * @LastEditors: 丁攀峰 allen@leanktech.com
- * @LastEditTime: 2023-08-30 13:02:07
+ * @LastEditTime: 2023-08-30 14:38:32
  * @FilePath: /demo/_worker.js
  */
 // 超级模式,
@@ -35,26 +35,25 @@ export default {
         const rewriter = new HTMLRewriter().on('div#dynamic-content', new ContentHandler())
         if (match && match[1]) {
             const kv_res = await env.KV.get(match[1]);
-            
             const sql = 'SELECT * FROM sloppyware_app_info LIMIT 5';
 	        const { d1_res } = await env.DB.prepare(sql).all();
             // 创建一个包含基础 HTML 结构的新 Response 对象
             const html = `
-          <!DOCTYPE html>
-          <html lang="en">
-          <head>
-            <meta charset="UTF-8">
-            <title>动态内容</title>
-          </head>
-          <body>
-            <div id="dynamic-content"></div>
-            <h1>KV</h1>
-            <div>${kv_res}</div>
-            <h1>D1</h1>
-            <pre>${JSON.stringify(d1_res, null, 2)}</pre>
-          </body>
-          </html>
-        `
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <title>动态内容</title>
+                    </head>
+                    <body>
+                        <div id="dynamic-content"></div>
+                        <h1>KV</h1>
+                        <div>${kv_res}</div>
+                        <h1>D1</h1>
+                        <pre>${JSON.stringify(d1_res, null, 2)}</pre>
+                    </body>
+                    </html>
+                `
             const init = {
                 headers: { 'content-type': 'text/html;charset=UTF-8' }
             }

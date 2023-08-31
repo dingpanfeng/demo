f2c2dae62c9a484a6dc0084b7eecae4c5205634a
丁攀峰 <allen@leanktech.com>
2023-08-31 15:20:35 +0800

    15

--- functions/apps/[id].js
+++ functions/apps/[id].js
@@ -2,16 +2,16 @@
  * @Author: 丁攀峰 allen@leanktech.com
  * @Date: 2023-08-22 10:24:50
  * @LastEditors: 丁攀峰 allen@leanktech.com
- * @LastEditTime: 2023-08-31 15:18:16
+ * @LastEditTime: 2023-08-31 15:19:35
  * @FilePath: /demo/functions/apps/[id].js
  */
 // https://developers.cloudflare.com/pages/platform/functions/routing/
 export async function onRequest(context) {
     let id = context.params.id
-    // const kv_res = await context.env.KV.get(id)
+    const kv_res = await context.env.KV.get(id)
     // // const sql = 'SELECT * from sloppyware_app_info'
     // const sql1 = 'SELECT * FROM sloppyware_app_info WHERE app_package = ?1'
-    // const d1_res = await context.env.DB.prepare(sql1).bind(id).all()
+    const d1_res = await context.env.DB.prepare(sql1).bind(id).all()
     // 创建一个包含基础 HTML 结构的新 Response 对象
     const html = `
             <!DOCTYPE html>
@@ -22,7 +22,8 @@ export async function onRequest(context) {
             </head>
             <body>
                 <div id="dynamic-content">${id}</div>
-                
+                <h1>KV</h1>
+                <div>${kv_res}</div>
             </body>
             </html>
 `

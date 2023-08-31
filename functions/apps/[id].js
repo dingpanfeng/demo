5d74ccd7ac7cc44d9ab0089cd16c0d0229b906c6
丁攀峰 <allen@leanktech.com>
2023-08-31 22:28:25 +0800

    16

--- functions/apps/[id].js
+++ functions/apps/[id].js
@@ -2,7 +2,7 @@
  * @Author: 丁攀峰 allen@leanktech.com
  * @Date: 2023-08-22 10:24:50
  * @LastEditors: 丁攀峰 allen@leanktech.com
- * @LastEditTime: 2023-08-31 22:24:57
+ * @LastEditTime: 2023-08-31 22:28:09
  * @FilePath: /demo/functions/apps/[id].js
  */
 // https://developers.cloudflare.com/pages/platform/functions/routing/
@@ -13,7 +13,8 @@ export async function onRequest(context) {
     const sql1 = 'SELECT * FROM sloppyware_app_info WHERE app_package = ?1'
     // const d1_res = await context.env.DB.prepare(sql1).bind(id).all()
     const d1_res = await context.env.DB.prepare(sql1).bind(id).first()
-    const data = d1_res.results[0]
+    // const data = d1_res.results[0]
+    //  <p>${data.id}-${data.app_name}</p>
     // 创建一个包含基础 HTML 结构的新 Response 对象
     const html = `
             <!DOCTYPE html>
@@ -24,7 +25,7 @@ export async function onRequest(context) {
             </head>
             <body>
                 <h1>数据绑定</h1>
-                <p>${data.id}-${data.app_name}</p>
+               
                 <div id="dynamic-content">${id}</div>
                 <h1>KV</h1>
                 <div>${kv_res}</div>

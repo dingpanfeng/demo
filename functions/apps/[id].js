862740ba9856c256a1f8423a44f18fd7e17909a7
丁攀峰 <allen@leanktech.com>
2023-08-31 22:37:18 +0800

    18

--- functions/apps/[id].js
+++ functions/apps/[id].js
@@ -12,7 +12,7 @@ export async function onRequest(context) {
     // const sql = 'SELECT * from sloppyware_app_info'
     const sql1 = 'SELECT * FROM sloppyware_app_info WHERE app_package = ?1'
     // const d1_res = await context.env.DB.prepare(sql1).bind(id).all()
-    const data = await context.env.DB.prepare(sql1).bind(id).first()
+    const d1_res = await context.env.DB.prepare(sql1).bind(id).first()
     // const data = d1_res.results[0]
     //  <p>${data.id}-${data.app_name}</p>
     // 创建一个包含基础 HTML 结构的新 Response 对象
@@ -25,7 +25,7 @@ export async function onRequest(context) {
             </head>
             <body>
                 <h1>数据绑定</h1>
-                <p>${data.id}-${data.app_name}</p>
+                <p>${d1_res.id}-${d1_res.app_name}</p>
                 <h2>${id}</h2>
                 <div id="dynamic-content"></div>
                 <h1>KV</h1>

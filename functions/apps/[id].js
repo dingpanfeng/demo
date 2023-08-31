3a4de42b5152aa1c4af97b333d7da3dce58f1513
丁攀峰 <allen@leanktech.com>
2023-08-31 15:18:50 +0800

    15

--- functions/apps/[id].js
+++ functions/apps/[id].js
@@ -2,16 +2,16 @@
  * @Author: 丁攀峰 allen@leanktech.com
  * @Date: 2023-08-22 10:24:50
  * @LastEditors: 丁攀峰 allen@leanktech.com
- * @LastEditTime: 2023-08-31 15:14:31
+ * @LastEditTime: 2023-08-31 15:18:16
  * @FilePath: /demo/functions/apps/[id].js
  */
 // https://developers.cloudflare.com/pages/platform/functions/routing/
 export async function onRequest(context) {
     let id = context.params.id
-    const kv_res = await context.env.KV.get(id)
-    // const sql = 'SELECT * from sloppyware_app_info'
-    const sql1 = 'SELECT * FROM sloppyware_app_info WHERE app_package = ?1'
-    const d1_res = await context.env.DB.prepare(sql1).bind(id).all()
+    // const kv_res = await context.env.KV.get(id)
+    // // const sql = 'SELECT * from sloppyware_app_info'
+    // const sql1 = 'SELECT * FROM sloppyware_app_info WHERE app_package = ?1'
+    // const d1_res = await context.env.DB.prepare(sql1).bind(id).all()
     // 创建一个包含基础 HTML 结构的新 Response 对象
     const html = `
             <!DOCTYPE html>
@@ -21,13 +21,8 @@ export async function onRequest(context) {
                 <title>动态内容</title>
             </head>
             <body>
-                <div id="dynamic-content"></div>
-                <h1>KV</h1>
-                <div>${kv_res}</div>
-                <h1>D1</h1>
-                <pre>${JSON.stringify(d1_res, null, 2)}</pre>
-                <h1>D1</h1>
-                <pre>${JSON.stringify(json, null, 2)}</pre>
+                <div id="dynamic-content">${id}</div>
+                
             </body>
             </html>
 `
@@ -37,3 +32,9 @@ export async function onRequest(context) {
         }
     })
 }
+{/* <h1>KV</h1>
+                <div>${kv_res}</div>
+                <h1>D1</h1>
+                <pre>${JSON.stringify(d1_res, null, 2)}</pre>
+                <h1>D1</h1>
+                <pre>${JSON.stringify(json, null, 2)}</pre> */}
